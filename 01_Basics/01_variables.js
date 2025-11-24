62f875e6ceb20e3bf2e3ba071c87d24cdf706553
ahmedrehman855-wq <ahmedrehman855@gmail.com>
2025-11-24 05:43:22 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14456
+let accountEmail= "ahmedrehman556@gmail.com"
+var accountPassword= "12345"
+accountCity= "Hyderabad"
+
+// accountId= 2 // not allowed
+accountEmail= "hc@hc.com"
+accountPassword= "212121"
+accountCity= "Jaipur"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
