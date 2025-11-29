9f046e8949eb2de87ecaeee934bed1bada2626eb
ahmedrehman855-wq <ahmedrehman855@gmail.com>
2025-11-29 04:42:28 +0000

    datatypes summary

---
+++ 01_Basics/datatypes.summary.js
@@ -0,0 +1,55 @@
+// # Primitive 
+
+//  7 types : String, Number, Boolearn, Null (empty), undefined, Symbol, BihInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggesIn = false
+const outsideTemp = null
+let userEmail
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 34567654567678678n 
+
+
+
+
+//  Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Rehman",
+    age: 22,
+}
+
+const myFunction = function (){console.log("Hello World");}
+
+console.log(typeof outsideTemp);
+
+
+
+
+
+
+// Return type of variables in JavaScript
+
+
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
