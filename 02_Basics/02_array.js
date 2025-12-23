a293def6defacf3e48dd853cfc6464586dbb0c71
ahmedrehman855-wq <ahmedrehman855@gmail.com>
2025-12-23 22:54:45 +0000

    02 array

---
+++ 02_Basics/02_array.js
@@ -0,0 +1,12 @@
+const marvel_heros = ["thor", "Ironman", "Spiderman"]
+const dc_heros = ["superman", "batman", "flash"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+
