2be4de4e73cb127dce5bb84198a205dd57812cf0
ahmedrehman855-wq <ahmedrehman855@gmail.com>
2025-12-25 23:20:43 +0000

    03 Objects

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,47 @@
+//singleton 
+// Object.create
+//object literals 
+const mySym = Symbol("Key1")
+
+const JsUser = {
+        name: "Rehman",
+        [mySym]: "mykey1", // must to put '[]' to change from string to symbol 
+        age: 18,
+        location: "Jaipur",
+        email: "Rehman@google.com",
+        isLoggesIn: false,
+        lastLoginDays: ["Monday", "Saturday"]
+    
+
+}
+
+// console.log(JsUser.email) // can't access when the email cis written in string in objects
+// console.log(JsUser["email"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "Rehman@chatgpt.com"
+// Object.freeze (JsUser) // if you don't want anyone to change email use freeze
+JsUser.email = "Rehman@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+    
+
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`)
+    
+
+}
+// console.log(JsUser.greeting);
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
+
+
+
+
+
+
+
