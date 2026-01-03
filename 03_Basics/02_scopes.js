43b6c7f6d6c41ec71322d736c78663900dc47dc4
ahmedrehman855-wq <ahmedrehman855@gmail.com>
2026-01-03 22:26:13 +0000

    02 scopes interesting

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -5,11 +5,48 @@ if(true){
     let a = 10 
     const b = 20  // the things written in curly brackets are block scopes and outside that it is called global scope
     c = 30
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
     
 } //{} this is called scope, using if{} and if else{} the curly brackets are called scopes
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+function one(){
+    const username = "Rehman"
+    function two(){
+        const website = "Hitesh"
+        console.log(username);
+        
+    }
+    // console.log(website)
+     two()
+}
+// one()
+
+if(true){
+    const username = "Rehman"
+    if(username === "Rehman"){
+        const website = " youtube"
+        // console.log(username + website);
+        
+
+    }
+    // console.log(website); /error
+    
+}
+// console.log(username); // error
+
+//+++++++++ interesting ++++++++++
+
+addone(5)
+function addone (num){
+    return num + 1
+}
+
+addTwo(5)         
+const addTwo = function (num){
+    return num + 2 
+
+}
