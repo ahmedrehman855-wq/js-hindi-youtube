3ddcb9d6bac91c0d475fe2451ba49e8c7c9497db
ahmedrehman855-wq <ahmedrehman855@gmail.com>
2026-01-07 23:18:43 +0000

    04 controlflow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,14 @@
+// if 
+
+const isUserLoggedIn = true
+const temperature = 41
+
+// if(temperature === 41){
+//     console.log("less than 50");
+    
+// } else {
+//     console.log("Greater than 50");
+    
+// }
+// console.log ("execute")
+// < , > ,<= ,>= ,==, !=, ===, !== // ! is named as not equal to 
