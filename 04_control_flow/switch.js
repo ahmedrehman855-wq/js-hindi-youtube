8c78ab21adfb2c6abc6c9a715adeda84cfc9b1a2
ahmedrehman855-wq <ahmedrehman855@gmail.com>
2026-01-19 23:44:11 +0000

    truthy

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -6,22 +6,48 @@
 //     default:
 //         break;
 // }
-const month = 3
+// const month = 3
+
+// switch (month) {
+//     case 1: 
+//         console.log("january");
+        
+//         break;
+//     case 2: 
+//         console.log("feb");
+        
+//         break;
+//     case 3: 
+//         console.log("march");
+        
+//         break;
+//     case 4: 
+//         console.log("april");
+        
+//         break;
+
+//     default:
+//         console.log("default case match");
+        
+//         break;
+// }
+
+const month = "march"
 
 switch (month) {
-    case 1: 
+    case "jan": 
         console.log("january");
         
         break;
-    case 2: 
+    case "feb": 
         console.log("feb");
         
         break;
-    case 3: 
+    case "march": 
         console.log("march");
         
         break;
-    case 4: 
+    case "april": 
         console.log("april");
         
         break;
