890173e71d695d1f6779f21c3de478cc43d7d03f
ahmedrehman855-wq <ahmedrehman855@gmail.com>
2026-01-20 01:07:52 +0000

    truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,13 @@
+// const userEmail = "r@Rehman.ai"
+const userEmail = []
+
+if(userEmail){
+    console.log("Got user email");
+    
+}else {
+    console.log("Don't have user Email");
+    
+}
+
+// falsy values
+// false, 0 , -0, BigInt 0n, "", null, undefined, NaN
