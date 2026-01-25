e29e4f28d99958cb726f315f8d36d0a66501ba30
ahmedrehman855-wq <ahmedrehman855@gmail.com>
2026-01-25 22:12:13 +0000

    while condition

---
+++ 05_iterations/two.js
@@ -0,0 +1,27 @@
+let index = 0
+
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 3
+// }
+let myArray = ["flash", "superman", "batman"]
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+    
+}
+// let score = 1
+
+// do {
+//     console.log(`score is ${score}`);
+//     score++
+    
+// } while (score <=10);
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score <=10);
