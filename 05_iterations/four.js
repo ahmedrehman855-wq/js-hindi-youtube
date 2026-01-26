01910b2265a5c2bbce2759092f8719d9980a0c20
ahmedrehman855-wq <ahmedrehman855@gmail.com>
2026-01-26 01:32:56 +0000

    five

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+const myObject = {
+    js: "javascript",
+    cpp: "c++",
+    rb: "ruby",
+    swift: "swift by apple",
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+    
+}
+
+const programming = ["js","cpp","rb","swift"]
+for (const key in programming) {
+    // onsole.log(programming[key]);
+    
+    
+}
+const map = new Map()
+map.set("IND","India")
+map.set("USA","Unites States of America")
+map.set("Fr","France")
+for (const key in map) {
+    console.log(key);
+    
+    
+    
+}
