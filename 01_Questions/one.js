bc585c927c5f849d4880eb2019723f6e8704836a
ahmedrehman855-wq <ahmedrehman855@gmail.com>
2026-01-26 06:04:55 +0000

    test

---
+++ 01_Questions/one.js
@@ -0,0 +1,14 @@
+// let x = 10;
+// x= 20;
+// const y=10;
+// y= 20;
+
+// let a = "10";
+// let b = 10
+// console.log(typeof b)
+// console.log(typeof a);
+
+console.log(5 == "5");
+console.log(5 === "5");
+
+
