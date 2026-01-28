2c11341790efc20f4cf6425c219e55bb0d25f457
ahmedrehman855-wq <ahmedrehman855@gmail.com>
2026-01-28 00:31:11 +0000

    object and array

--- 01_Questions/one.js
+++ 01_Questions/one.js
@@ -8,7 +8,42 @@
 // console.log(typeof b)
 // console.log(typeof a);
 
-console.log(5 == "5");
-console.log(5 === "5");
+// console.log(5 == "5");
+// console.log(5 === "5");
 
+// 0==false
+// 0===false
 
+// for (let i = 0; i < 3; i++){
+//     console.log(i);
+// }
+
+// function demo(){
+//     return 5;
+    
+// }
+// let x = demo()
+// console.log(x);
+
+// let numbers = [10,20,30]
+// // numbers[1]
+// let num = [1,2,3,4,5]
+// let doubled = num.map( n => n * 2
+// )
+// console.log(doubled);
+
+// let event = num.filter(n => n % 2===0)
+// console.log(event);
+let user = {
+    name: "Ahmed",
+    skills: ["js","html"]
+}
+console.log(`${user.name} knows js`);
+
+// let double = num.map(n => n * 2)
+// console.log(double);
+
+// let num = [5,12,18,3]
+
+// let even = num.filter(n => n % 2===0)
+// console.log(even);
