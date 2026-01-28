57177a95fb1b1129093fb343a05ca9228b18c1f1
ahmedrehman855-wq <ahmedrehman855@gmail.com>
2026-01-28 02:02:44 +0000

    Eight Reduce

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -6,3 +6,15 @@
 // } )
 // console.log(value);
  
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+
+// const newNum= myNums.filter(n => n>4)
+// const newNum= myNums.filter((num)=>{
+//     return num>4
+// } 
+// ) 
+// console.log(newNum);
+
+ 
