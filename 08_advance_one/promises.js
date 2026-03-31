898164c11ca074201d03b72f4223fd4c4948a266
ahmedrehman855-wq <ahmedrehman855@gmail.com>
2026-03-31 23:43:30 +0000

    promises

---
+++ 08_advance_one/promises.js
@@ -0,0 +1,83 @@
+const promiseOne = new Promise(function(resolve, reject){
+    // Do an async task
+    // DB calls, cryptogrraphy, network
+    setTimeout(function(){
+        console.log('Async task is complete');
+        resolve()
+        
+    },1000)
+})
+
+promiseOne.then(function(){
+    console.log('Promise consumed');
+    
+})
+
+new Promise(function(reslove, reject){
+    setTimeout(function(){
+        console.log('Async task 2');
+        reslove()
+        
+    },1000)
+}).then(function(){
+    console.log('Async 2 complete');
+    
+})
+
+const promiseThree = new Promise(function(reslove, reject){
+    setTimeout(function(){
+        reslove({username: "chai" , email: "chai@example.com"})
+    },1000)
+})
+promiseThree.then(function(user){
+    console.log(user);
+    
+})
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "chai", password: "12"})
+        }else{
+            reject('ERROR: Something went wrong')
+        }
+    },1000)
+})
+
+promiseFour.then((user) => {
+    console.log(user);
+    return user.username
+}).then((username)=>{
+    console.log(username);
+    
+}).catch(function(error){
+    console.log(error);
+    
+}).finally(()=> console.log("The Promise is either resolved or rejected"));
+
+
+const promiseFive = new Promise(function(resolve, reject){
+     setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "JavaScript", password: "12"})
+        }else{
+            reject('ERROR: JS went wrong')
+        }
+    },1000)
+
+})
+
+async function ConsumedPromiseFive(){
+    try { const response = await promiseFive
+        console.log(response);
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+ConsumedPromiseFive()
+
+
